4c5f191e24c45432cd1b05c88db16da51e812737
Code-181p <nguyenthanhcuong12a5chilang@gmail.com>
2024-02-26 15:19:08 +0700

    first commit

---
+++ TNS_Lib/TNS_Sensor_AccelGyro/SNO_MPU6050_AccessData.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TNS.Sensor.AccelGyro
+{
+    public class SNO_MPU6050_AccessData
+    {
+        public static DataTable List()
+        {
+            DataTable zTable = new DataTable();
+            string zSQL = "exec SP_SNPO_MPU6050_Search null,null,null,null";
+            string zConnectionString = TNS_DBConnection.Connecting.SQL_MainDatabase;
+            try
+            {
+                SqlConnection zConnect = new SqlConnection(zConnectionString);
+                zConnect.Open();
+                SqlCommand zCommand = new SqlCommand(zSQL, zConnect);
+                SqlDataAdapter zAdapter = new SqlDataAdapter(zCommand);
+                zAdapter.Fill(zTable);
+                zCommand.Dispose();
+                zConnect.Close();
+            }
+            catch (Exception ex)
+            {
+                string zstrMessage = ex.ToString();
+            }
+            return zTable;
+        }
+        public static DataTable Search(string fromDate, string toDate)
+        {
+            DataTable zTable = new DataTable();
+            string zSQL = "SELECT * FROM [TNS_VietTech].[dbo].[SNO_MPU6050] WHERE TimeDate BETWEEN @TuNgay AND @DenNgay";
+            string zConnectionString = TNS_DBConnection.Connecting.SQL_MainDatabase;
+            try
+            {
+                using (SqlConnection zConnect = new SqlConnection(zConnectionString))
+                {
+                    zConnect.Open();
+                    SqlCommand zCommand = new SqlCommand(zSQL, zConnect);
+                    zCommand.CommandType = CommandType.Text;
+                    zCommand.Parameters.Add("@TuNgay", SqlDbType.DateTime).Value = DateTime.Parse(fromDate);
+                    zCommand.Parameters.Add("@DenNgay", SqlDbType.DateTime).Value = DateTime.Parse(toDate);
+                    SqlDataAdapter adapter = new SqlDataAdapter(zCommand);
+                    adapter.Fill(zTable);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+            }
+          
+            return zTable;
+        }
+        public static DataTable Search2(string Date)
+        {
+            DataTable zTable = new DataTable();
+            string zSQL = "SELECT * FROM [TNS_VietTech].[dbo].[SNO_MPU6050] WHERE TimeDate = @TimeDate ";
+            string zConnectionString = TNS_DBConnection.Connecting.SQL_MainDatabase;
+            try
+            {
+                using (SqlConnection zConnect = new SqlConnection(zConnectionString))
+                {
+                    zConnect.Open();
+                    SqlCommand zCommand = new SqlCommand(zSQL, zConnect);
+                    zCommand.CommandType = CommandType.Text;
+                    zCommand.Parameters.Add("@TimeDate", SqlDbType.DateTime).Value = DateTime.Parse(Date);
+                    SqlDataAdapter adapter = new SqlDataAdapter(zCommand);
+                    adapter.Fill(zTable);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+            }
+
+            return zTable;
+        }
+    }
+}
---
+++ TNS_Lib/TNS_Sensor_AccelGyro/SNO_MPU6050_Record.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNS.Sensor.AccelGyro
+{
+    public class SNO_MPU6050_Record
+    {
+        #region [ Field Name ]
+        public string _AutoKey = "";
+        public string _Address = "";
+        public readonly DateTime? _TimeDate = null;
+        public float _GyroX = 0;
+        public float _GyroY= 0;
+        public float _GyroZ = 0;
+        public float _AccelX = 0;
+        public float _AccelY = 0;
+        public float _AccelZ = 0;
+        private string _Message = "";   
+        #endregion
+
+        public SNO_MPU6050_Record(string AutoKey)
+        {
+            string zSQL = "SELECT * FROM [dbo].[SNO_MPU6050] WHERE AutoKey = @Autokey";
+            string zConnectionString = TNS_DBConnection.Connecting.SQL_MainDatabase;
+            SqlConnection zConnect = new SqlConnection(zConnectionString);
+            zConnect.Open();
+            try
+            {
+                SqlCommand zCommand = new SqlCommand(zSQL, zConnect);
+                zCommand.CommandType = CommandType.Text;
+                zCommand.Parameters.Add("@AutoKey", SqlDbType.UniqueIdentifier).Value = new Guid(AutoKey);
+                SqlDataReader zReader = zCommand.ExecuteReader();
+                if (zReader.HasRows)
+                {
+                    zReader.Read();
+                    _AutoKey = zReader["AutoKey"].ToString();
+                    _Address = zReader["Address"].ToString();
+
+                    
+                    _GyroX = float.Parse(zReader["Gyro_X"].ToString());
+                    _GyroY = float.Parse(zReader["Gyro_Y"].ToString());
+                    _GyroZ = float.Parse(zReader["Gyro_Z"].ToString());
+                    
+                    _AccelX = float.Parse(zReader["Accel_X"].ToString());
+                    _AccelY = float.Parse(zReader["Accel_Y"].ToString());
+                    _AccelZ = float.Parse(zReader["Accel_Z"].ToString());
+                    
+                    if (zReader["TimeDate"] != DBNull.Value)
+                        _TimeDate = (DateTime)zReader["TimeDate"];
+                    else
+                        _Message = "OK";
+                }
+                else
+                {
+                    _Message = "404 Not Found";
+                }
+
+                zReader.Close();
+                zCommand.Dispose();
+            }
+            catch (Exception Err)
+            {
+                _Message = "501 " + Err.ToString();
+            }
+            finally
+            {
+                zConnect.Close();
+            }
+        }
+    }
+}
